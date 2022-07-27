3f92b3f559eda0ce0d214ae8230cd55356edffe5
Luca G <luca.p.giannotti@gmail.com>
2022-07-26 23:25:02 -0500

    Finished challenges 1 & 2 | Chapter 2

---
+++ challenges.js
@@ -0,0 +1,24 @@
+// |    Chapter 2
+//  Exercise 1: Looping a triangle
+let triangle = "#"
+while (triangle.length<8) {
+    console.log(triangle)
+    triangle += "#"
+}
+
+//  Exercise 2: FizzBuzz
+for (let FizzBuzz=1; FizzBuzz <= 100; FizzBuzz+=1) {
+    if (FizzBuzz % 3 == 0 && FizzBuzz % 5 == 0) {
+        console.log("FizzBuzz")
+    }
+    else if (FizzBuzz % 5 == 0) {
+        console.log("Buzz")
+    }
+    else if (FizzBuzz % 3 == 0) {
+        console.log("Fizz")
+    }
+    else {
+        console.log(FizzBuzz)
+    }
+}
+//  Exercise 3: Checkerboard
