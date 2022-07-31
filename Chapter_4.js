e06d544c228ed32f08dd7bd5a4a9f4a91e7d44fd
Luca G <luca.p.giannotti@gmail.com>
2022-07-31 02:15:41 -0500

    Finished Chapter_4.1

--- Chapter_4.js
+++ Chapter_4.js
@@ -1,21 +1,27 @@
 //  Exercise 1: The Sum of a Range
-function range(n1, n2) {
+function range(n1, n2, step = 1) {
     let ans = []
-    for (let index = n1; index <= n2; index += 1) {
-        ans.push(index);
+    if (step > 0) {
+        for (let index = n1; index <= n2; index += step) {
+            ans.push(index);
+        }
+    }
+    else {
+        for (let index = n2; index >= 1; index += step) {
+            ans.push(index)
+        }
     }
     return ans
 }
-console.log(range(1,10))
-function sum(n1, n2) {
-    return range(n1, n2)
-    let ans2 = 0
-    for (index = 1; index <= ans.length ; index++) {
-        ans2 = ans2 + ans[index]
+console.log(range(1,10,-2))
+function sum(range) {
+    let sums = 0
+    for (let index of range) {
+        sums += index
     }
-    return ans2
+    return sums
 }
-console.log(sum(1,10))
+console.log(sum(range(1,10)))
 //  Exercise 2: 
 
 //  Excerise 3: 
